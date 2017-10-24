0a96806e48b565a96588b9eb99e950e9e106c375
Alex Begler <beglerah@s.dcsdk12.org>
2017-10-24 08:11:54 -0600

    def add_one

--- iteration.py
+++ iteration.py
@@ -3,6 +3,7 @@
 # Make a change from home
 
 # iteration pattern
+# doing the same thing once for each item in the list
 
 # [1, 5, 7 ,8 , 4, 3]
 
@@ -14,6 +15,12 @@ def iterate(list):
 	# for each loop
 	for item in list:
 		print item
+
+def add_one(list):
+	for i in range(0, len(list)):
+		list[i] += 1
+
+	return list
 
 def print_scores(names, scores):
 	for i in range(0, len(names)):
